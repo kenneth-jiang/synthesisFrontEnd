73f9c318824e6bea3c737705e899e5b1a6966bb4
laurkim <laurakeem@gmail.com>
2017-11-30 14:38:16 -0500

    create spotify container

---
+++ src/Components/SpotifyContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SpotifyContainer extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Inside Spotify Container</h3>
+      </div>
+    )
+  }
+}
+
+export default SpotifyContainer;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import SpotifyContainer from './Components/SpotifyContainer.js';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+  <SpotifyContainer />,
+  document.getElementById('root')
+);
+// registerServiceWorker();
