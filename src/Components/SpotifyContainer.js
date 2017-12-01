c5a899f3188d3074e211465ec53e7ec66edfd9e7
laurkim <laurakeem@gmail.com>
2017-11-30 19:24:44 -0500

    loads login and redirects

--- src/Components/SpotifyContainer.js
+++ src/Components/SpotifyContainer.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import { Button } from 'semantic-ui-react'
+import { Route } from 'react-router-dom';
+
+const button = () => {
+  return (
+    <Button as="a" href="http://localhost:3000/api/v1/login">Log In</Button>
+  )
+}
 
 class SpotifyContainer extends Component {
   render() {
     return (
       <div>
-        <h3>Inside Spotify Container</h3>
+        <Route exact path="/login" component={button} />
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import SpotifyContainer from './Components/SpotifyContainer.js';
 
+const MyApp = ()=>(
+  <Router>
+    <SpotifyContainer />
+  </Router>
+)
+
+
 ReactDOM.render(
-  <SpotifyContainer />,
+  <MyApp />,
   document.getElementById('root')
 );
 // registerServiceWorker();
