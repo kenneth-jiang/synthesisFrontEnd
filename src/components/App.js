ae17953e1580e23c44d98f210df625f61060b7d7
laurkim <laurakeem@gmail.com>
2017-12-06 21:13:38 -0500

    import stylesheet to App

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route } from 'react-router-dom'
 import AuthorizationPage from '../components/1 Login/AuthorizationPage';
+import '../app.css'
 
 class App extends React.Component {
   render() {
