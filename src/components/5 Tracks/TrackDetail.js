f4c9ba6a4c894a97f74df63ff3dac7209c98e5c8
laurkim <laurakeem@gmail.com>
2017-12-06 22:58:56 -0500

    refactor song length and add styling to track detail

--- src/components/5 Tracks/TrackDetail.js
+++ src/components/5 Tracks/TrackDetail.js
@@ -1,14 +1,23 @@
 import React from 'react';
-import { Button } from 'semantic-ui-react'
+import { Button, Header, Icon } from 'semantic-ui-react'
 
 const TrackDetail = (props) => {
+  const convertTime = (milliseconds) => {
+    return parseInt(milliseconds / 1000 / 60) + ":" + parseInt(milliseconds / 1000 % 60);
+  }
+
   return (
     <li>
       <ul>
-        {props.trackDetail.name} <br/>
+          <Header as='h3'>
+            <Header.Content>
+              <Icon name='music' />
+              {props.trackDetail.name}
+            </Header.Content>
+          </Header>
         Artist: {props.trackDetail.artists.map((artist) => {return artist.name}).join(', ')} <br/>
-        Length: {((props.trackDetail.duration_ms)/60000)} <br/>
-        Popularity: {props.trackDetail.popularity} <br/><br/>
+        Length: {convertTime(props.trackDetail.duration_ms)} <br/>
+        Popularity: {props.trackDetail.popularity}% <br/><br/>
         <Button size="mini" onClick={props.fetchRelatedArtists} value={props.trackDetail.artists[0].id}>Related Artists</Button> <br/>
         <Button size="mini" onClick={props.handleUri} value={props.trackDetail.uri}>Play Song</Button> <br/><br/>
       </ul>
